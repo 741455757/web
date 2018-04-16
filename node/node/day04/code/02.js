6986ad38390ddd2b7b5bba64cdd69415feee4fd3
Jessica Wang <741455757@qq.com>
2018-04-16 10:55:42 +0800

    Create 02.js
    __dirname不能乱用

---
+++ node/node/day04/code/02.js
@@ -0,0 +1,12 @@
+//__dirname不能乱用
+
+var fs = require("fs");
+var path = require("path");
+
+var mkdirs = require("./modules/mkdirs.js");
+
+
+mkdirs(path.join(__dirname, 'f1/f2/f3'));
+mkdirs(path.join(__dirname, 'f1/f2/f3/f4'),(err) => {
+    if(err) console.log(err);
+})
