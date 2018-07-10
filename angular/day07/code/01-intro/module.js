f18843e822384b1f6f3cec58c385123589fb904d
Jessica Wang <741455757@qq.com>
2018-07-10 08:07:25 +0800

    模块化的演变过程

---
+++ angular/day07/code/01-intro/module.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Jessica Wang
+* @Date:   2018-07-08 19:38:19
+* @Last Modified by:   Jessica Wang
+* @Last Modified time: 2018-07-08 19:39:44
+*/
+var module = function(){
+	console.log("i am a singer");
+}
---
+++ angular/day07/code/01-intro/tab.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Jessica Wang
+* @Date:   2018-07-08 19:38:05
+* @Last Modified by:   Jessica Wang
+* @Last Modified time: 2018-07-08 19:43:04
+*/
+var module = function(){
+	console.log("i am a dancer");
+}
