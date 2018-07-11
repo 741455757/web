1de81e366bc90274a6f952d7695e8d77359f655a
Jessica Wang <741455757@qq.com>
2018-07-11 21:39:29 +0800

    seajs配置

---
+++ angular/day07/code/03-seajs/04-main.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Jessica Wang
+* @Date:   2018-07-10 23:56:48
+* @Last Modified by:   Jessica Wang
+* @Last Modified time: 2018-07-10 23:57:51
+*/
+
+define(function(require,exports,module){
+	//想要使用jquery怎么办
+})
---
+++ angular/day07/code/03-seajs/modules/calc.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Jessica Wang
+* @Date:   2018-07-11 19:23:16
+* @Last Modified by:   Jessica Wang
+* @Last Modified time: 2018-07-11 19:51:07
+*/
+'use strict';
+
+
+define(function(require,exports,module){
+	console.log(111);
+})
