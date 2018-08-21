e8c69bba21ac5eb8d9fe930ea5d7fe9dbe74718c
Jessica Wang <7414557@qq.com>
2018-08-21 23:27:24 +0800

    new file:   .babelrc 	new file:   package.json 	new file:   src/App.vue 	new file:   src/index.html 	new file:   src/index.js 	new file:   src/main/Account.vue 	new file:   src/main/GoodsList.vue 	new file:   src/subcom/login.vue 	new file:   src/subcom/register.vue 	new file:   webpack.config.js

---
+++ vue/day06/05.webpack-study/src/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+
+// 1.导入vue-router包
+import VueRouter from 'vue-router'
+
+// 2.手动安装VueRouter
+Vue.use(VueRouter)
+
+// 导入app组件
+import app from './App.vue'
+import account from './main/Account.vue'
+import goodslist from './main/GoodsList.vue'
+// 导入Account的两个子组件
+import login from './subcom/login.vue'
+import register from './subcom/register.vue'
+// 3.创建漏由对象
+var router = new VueRouter({
+    routes:[
+        //account goodslist
+        {
+            path:'/account',
+            component:account,
+            children:[
+                {path:'login',component:login},
+                {path:'register',component:register}
+            ]
+        },
+        {path:'/goodslist',component:goodslist},
+    ]
+})
+var vm = new Vue({
+    el:"#app",
+    render:function(createElements){//renter 会把el指定的容器中，所有都清空覆盖，所有不要把路由router-view和router-link直接写到el所控制的元素中
+        return createElements(app)
+    },
+    router //4.将路由对象挂载到vm上
+})
+
+// 注意：APP这个组件，是通过VM实例的render函数，渲染出来的，render函数如果要渲染组件，渲染出来的组件，只能放到el：#app 所指定的元素中；
+// Acoount和GoodsList组件，是通过路由匹配监听到的，所以，这两个组件，只能展示到属于路由 <router-view></router-view>中去
---
+++ vue/day06/05.webpack-study/src/main/Account.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>这是Account组件</h1>
+        <router-link to="/account/login">登录</router-link>
+        <router-link to="/account/register">注册</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    
+</script>
+<style lang="scss" scoped>
+/* 普通的style标签只支持普通的样式，如果想启用scss或less，需要为style元素，设置lang属性 */
+// 只要咱们的style标签，是在.vue组件中定义的，那么都推荐style开启scoped属性  
+    body{
+        div {
+            font-style: italic
+        }
+    }
+</style>
