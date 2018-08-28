5d2bddd111def46c8c8bf463bc66ec3e37faa060
Jessica Wang <7414557@qq.com>
2018-08-28 23:59:47 +0800

    绘制新闻资讯列表页面布局

--- vue/day08/02.vue-cms/src/components/news/NewsList.vue
+++ vue/day08/02.vue-cms/src/components/news/NewsList.vue
@@ -1,11 +1,37 @@
 <template>
     <div>
+        <ul class="mui-table-view">
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://avatars1.githubusercontent.com/u/19582642?s=40&v=4">
+						<div class="mui-media-body">
+                            <h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                                <span>发表时间: 2012-12 12:12:12</span>
+                                <span>点击：0次</span>
+                            </p>
+						</div>
+					</a>
+				</li>
+			</ul>
         <h1> 新闻列表</h1>
     </div>
 </template>
 <script>
     
 </script>
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+    .mui-table-view{
+        li{
+            h1{
+                font-size:14px;
+            }
+            .mui-ellipsis{
+                font-size: 12px;
+                color: #226aff;
+                display: flex;
+                justify-content: space-between;
+            }
+        }
+    }
 </style>
