af79bfdd5de6d22f1c272a29216bcbb365d872f2
Jessica Wang <7414557@qq.com>
2018-10-13 17:37:21 +0800

    事件绑定

--- angular5/day01/todomvc-angular/src/app/app.component.ts
+++ angular5/day01/todomvc-angular/src/app/app.component.ts
@@ -24,4 +24,23 @@ export class AppComponent {
     title:string,
     done: boolean
   }[] = todos;
+
+  addTodo(e):void {
+    // console.log(e.target.value);e.target dom元素
+    // console.log(e.keyCode); 事件修饰符
+    // console.log("添加方法被调用了");
+    const titleText = e.target.value;
+    if(!titleText.length){
+      return;
+    }
+    const last = this.todos[this.todos.length - 1];
+    this.todos.push({
+      id: last?last.id + 1:1,
+      title: titleText,
+      done:false
+    });
+    // 清除文本框
+    e.target.value ='';
+    console.log(this.todos);
+  }
 }
