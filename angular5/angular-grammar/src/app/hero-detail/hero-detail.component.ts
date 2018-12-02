d65589a3e1b06088977a2aae93829b0a8260f6fe
Jessica Wang <7414557@qq.com>
2018-12-02 22:52:38 +0800

    http 利用本地web-api实现

--- angular5/angular-grammar/src/app/hero-detail/hero-detail.component.ts
+++ angular5/angular-grammar/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit ,Input} from '@angular/core';
 import {Hero} from '../Hero';
+import { ActivatedRoute } from '@angular/router';
+import {Location} from '@angular/common';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -7,10 +10,29 @@ import {Hero} from '../Hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero;
-  constructor() { }
+  // @Input() hero: Hero;
+  hero: Hero;
+  constructor(
+   private route: ActivatedRoute,
+   private location: Location,
+   private heroService: HeroService
+  ) { }
 
   ngOnInit() {
+    this.getHero();
   }
 
+  getHero(): void{
+    const  id: number = (Number)(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero)
+  }
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+  goBack(): void {
+    this.location.back();
+  }
+  
 }
